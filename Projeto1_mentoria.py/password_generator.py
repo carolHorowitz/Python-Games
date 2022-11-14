8e46d96b0b5dff3d4dec9b6e56158dd1802a449e
carolHorowitz <carolvet.horowitz@gmail.com>
2022-11-14 13:11:34 -0300

    First commit containing initial lines of code.

--- Projeto1_mentoria.py/password_generator.py
+++ Projeto1_mentoria.py/password_generator.py
@@ -1,9 +1,9 @@
 
 
-def gerador_de_senha():
+def password_generator():
 
     print("Welcome!! Let´s start to create a new password.")
-    characters = int(input("How many character should there be?: "))
+    characters = int(input("How many character should there be? Your password must contain a minimum of 4 and a maximum of 10 characters: "))
     upper = input("Should it have uppercase letter?; ")
     num = input("Should it have numbers?: ")
     esp = input("Should it have specials characters?: ")
@@ -11,5 +11,5 @@ def gerador_de_senha():
     password_length = number_of_characters(characters)
     
 def number_of_characters(characters):
-    return ('_' for )
+    
         
